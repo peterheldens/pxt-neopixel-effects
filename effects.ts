ca599f1ab0de29acee166e0648d912bdd5d29602
peterheldens <peter.heldens@live.nl>
2016-07-27 21:29:16 -0700

    adding pain function

--- effects.ts
+++ effects.ts
@@ -1,9 +1,20 @@
-namespace neopixel {
+//%
+enum Colors {
+    Black = 0,
+    White = ~0x0,
+    Blue = 0x0000ff00
+}
+namespace neopixelEffects {
     /**
      * A sample image
      */
-    //% blockId="neopixel_effects_rainbow" block="neopixel|%strip|rainbow"
-    export function rainbow(st: Strip) {
+    //% blockId="neopixel_effects_rainbow" block="neopixel|%strip|paint %c=neopixel_effects_colors"
+    export function paint(st: Strip, c: number) {
 
+    }
+
+    //% block=neopixel_effects_colors block="%c"
+    export function colors(c: Colors): number {
+        return <number>c;
     }
 }
