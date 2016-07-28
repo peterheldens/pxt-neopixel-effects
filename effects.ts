813563590f794010f5409505be887b682cf765d6
peterheldens <peter.heldens@live.nl>
2016-07-27 21:37:32 -0700

    fixing syntax issues

--- effects.ts
+++ effects.ts
@@ -4,17 +4,19 @@ enum Colors {
     White = ~0x0,
     Blue = 0x0000ff00
 }
-namespace neopixelEffects {
-    /**
-     * A sample image
-     */
-    //% blockId="neopixel_effects_rainbow" block="neopixel|%strip|paint %c=neopixel_effects_colors"
-    export function paint(st: Strip, c: number) {
-
-    }
-
-    //% block=neopixel_effects_colors block="%c"
+namespace neopixel {
+    //% blockId=neopixel_effects_colors block="%c"
+    //% weight=1
     export function colors(c: Colors): number {
         return <number>c;
+    }
+
+    /**
+     * Set a color on the strip
+     */
+    //% blockId="neopixel_effects_rainbow" block="neopixel|%strip|paint %c=neopixel_effects_colors"
+    //% weight=20
+    export function paint(st: Strip, c: number) {
+
     }
 }
